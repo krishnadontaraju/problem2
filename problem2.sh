bea8098b7427b96d75b6e92810c4234589769eaa
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-09 19:53:26 +0530

    [sai] looped the flip

--- problem2.sh
+++ problem2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
-if [ (( RANDOM % 2 )) -eq 0 ]
+read -p "GOW MANY TIMES TO FLIP THE COIN ?	" user_input
+for (( i=0; i<$user_input; i++ ))
+do
+if [ $(( RANDOM % 2 )) -eq 0 ]
 then
 	echo "HEADS"
 else
 	echo "TAILS"
 fi
-
+done
