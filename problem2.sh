d4506b231c5fb39043e3ff64eb7a28f748089577
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-09 21:07:13 +0530

    [SAI] added the tie condition and modified the code accordingly

--- problem2.sh
+++ problem2.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-heads_count=0
-tails_count=0
+heads_count=
+tails_count=
 while [ $heads_count -le 20 -a $tails_count -le 20 ]
 do
-		if [ $heads_count -eq 21 -a $tails_count -eq 21 ]
-		then
 		if [ $(( RANDOM % 2 )) -eq 0 ]
 		then
 			heads_count=$(( heads_count + 1 ))
@@ -13,15 +11,34 @@ do
 			tails_count=$(( tails_count + 1 ))
 		fi
 done
-if [ $heads_count -eq 21 ]
+if [ $heads_count -gt $tails_count ]
 then
 	echo "HEADS - $heads_count WON WITH $(( heads_count - tails_count )) MARGIN "
 else
 	echo "TAILS - $tails_count WON WITH $(( tails_count -heads_count )) MARGIN "
 fi
+if [ $heads_count -eq $tails_count ]
+then
+	head_difference=0
+	tails_difference=0
+while [ $head_difference -le 2 -a $tails_difference -le 2 ]
+do
+	if [ $(( RANDOM % 2 )) -eq 0 ]
+                then
+                        heads_count=$(( heads_count + 1 ))
+			echo "Heads"
+                else
+                        tails_count=$(( tails_count + 1 ))
+			echo "Tails"
+                fi
+		head_difference=$(( heads_count - tails_count ))
+        	tails_difference=$(( tails_count - heads_count ))
+done
 
-
-
-
-
-
+if [ $heads_count -gt $tails_count ]
+then
+        echo "HEADS - $heads_count WON WITH $(( heads_count - tails_count )) MARGIN "
+else
+        echo "TAILS - $tails_count WON WITH $(( tails_count -heads_count )) MARGIN "
+fi
+fi
